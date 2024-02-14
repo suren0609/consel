266baff664772873941466f995776d7845124360
suren0609 <suren.balayan0609@gmail.com>
2024-02-14 18:37:22 +0400

    Writed responsive design

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -9,6 +9,7 @@ import partner3 from "../assets/Group 5297.png";
 import styles from "./HomePage.module.scss";
 import ProductCard from "../components/ProductCard";
 import ServiceSection from "../components/ServiceSection";
+import partnersGroup from "../assets/Group 5298.png";
 
 const responsive = {
   desktop: {
@@ -16,8 +17,13 @@ const responsive = {
     items: 4,
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
+    breakpoint: { max: 1024, min: 740 },
     items: 3,
+  },
+
+  littleTablet: {
+    breakpoint: { max: 740, min: 464 },
+    items: 2,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
@@ -44,6 +50,7 @@ const HomePage = () => {
             <img src={partner4} alt="" />
             <img src={partner5} alt="" />
           </div>
+          <img className={styles.partnersMob} src={partnersGroup} alt="" />
         </div>
       </div>
       <div className={styles.newCollection}>
@@ -58,6 +65,27 @@ const HomePage = () => {
               <li>Անվտանգություն</li>
             </ul>
             <hr />
+          </div>
+          <div className={styles.categoriesSelect}>
+            <p>Օդակարգավորում</p>
+            <div className={styles.arrowDown}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="8"
+                viewBox="0 0 14 8"
+                fill="none"
+              >
+                <path
+                  d="M13.0002 1L7.00024 7L1.00024 1"
+                  stroke="#3E4095"
+                  stroke-width="2"
+                  stroke-miterlimit="10"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
           </div>
           <div className={styles.slider}>
             <Carousel
@@ -81,6 +109,116 @@ const HomePage = () => {
         </div>
       </div>
       <ServiceSection />
+      <div className={styles.homeImage}>
+        <div id={styles.one} className={styles.circle}>
+          1
+          <div className={styles.imagePopup}>
+            <h4>Ապրանքի անվանում</h4>
+            <p>
+              Մոդելների լայն տեսականին ներառում է հինգ մոդել 2,6-ից մինչև 7 կՎտ
+              հզորությամբ մինչև 20-ից մինչև 70 ք.մ.
+            </p>
+            <a href="#">
+              <span>Կարդալ ավելին</span>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="14"
+                  viewBox="0 0 24 14"
+                  fill="none"
+                >
+                  <path
+                    d="M0 7.00002C0 7.26523 0.105357 7.51959 0.292893 7.70712C0.48043 7.89466 0.734784 8.00002 1 8.00002H20.586L16.294 12.292C16.1118 12.4806 16.011 12.7332 16.0133 12.9954C16.0156 13.2576 16.1208 13.5084 16.3062 13.6938C16.4916 13.8792 16.7424 13.9844 17.0046 13.9867C17.2668 13.989 17.5194 13.8882 17.708 13.706L23.708 7.70602C23.8005 7.61402 23.874 7.50464 23.9241 7.38417C23.9742 7.26369 24 7.1345 24 7.00402V7.00002C24 6.87002 23.974 6.74002 23.922 6.61802C23.8722 6.49678 23.7987 6.38665 23.706 6.29402L17.706 0.294017C17.5174 0.111859 17.2648 0.011065 17.0026 0.0133435C16.7404 0.0156219 16.4896 0.120791 16.3042 0.306199C16.1188 0.491607 16.0136 0.74242 16.0113 1.00462C16.009 1.26681 16.1098 1.51942 16.292 1.70802L20.586 6.00002H1C0.734784 6.00002 0.48043 6.10537 0.292893 6.29291C0.105357 6.48045 0 6.7348 0 7.00002Z"
+                    fill="#3E4095"
+                  />
+                </svg>
+              </span>
+            </a>
+          </div>
+        </div>
+        <div id={styles.two} className={styles.circle}>
+          2
+          <div className={styles.imagePopup}>
+            <h4>Ապրանքի անվանում</h4>
+            <p>
+              Մոդելների լայն տեսականին ներառում է հինգ մոդել 2,6-ից մինչև 7 կՎտ
+              հզորությամբ մինչև 20-ից մինչև 70 ք.մ.
+            </p>
+            <a href="#">
+              <span>Կարդալ ավելին</span>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="14"
+                  viewBox="0 0 24 14"
+                  fill="none"
+                >
+                  <path
+                    d="M0 7.00002C0 7.26523 0.105357 7.51959 0.292893 7.70712C0.48043 7.89466 0.734784 8.00002 1 8.00002H20.586L16.294 12.292C16.1118 12.4806 16.011 12.7332 16.0133 12.9954C16.0156 13.2576 16.1208 13.5084 16.3062 13.6938C16.4916 13.8792 16.7424 13.9844 17.0046 13.9867C17.2668 13.989 17.5194 13.8882 17.708 13.706L23.708 7.70602C23.8005 7.61402 23.874 7.50464 23.9241 7.38417C23.9742 7.26369 24 7.1345 24 7.00402V7.00002C24 6.87002 23.974 6.74002 23.922 6.61802C23.8722 6.49678 23.7987 6.38665 23.706 6.29402L17.706 0.294017C17.5174 0.111859 17.2648 0.011065 17.0026 0.0133435C16.7404 0.0156219 16.4896 0.120791 16.3042 0.306199C16.1188 0.491607 16.0136 0.74242 16.0113 1.00462C16.009 1.26681 16.1098 1.51942 16.292 1.70802L20.586 6.00002H1C0.734784 6.00002 0.48043 6.10537 0.292893 6.29291C0.105357 6.48045 0 6.7348 0 7.00002Z"
+                    fill="#3E4095"
+                  />
+                </svg>
+              </span>
+            </a>
+          </div>
+        </div>
+        <div id={styles.three} className={styles.circle}>
+          3
+        </div>
+        <div id={styles.four} className={styles.circle}>
+          4
+          <div className={styles.imagePopup}>
+            <h4>Ապրանքի անվանում</h4>
+            <p>
+              Մոդելների լայն տեսականին ներառում է հինգ մոդել 2,6-ից մինչև 7 կՎտ
+              հզորությամբ մինչև 20-ից մինչև 70 ք.մ.
+            </p>
+            <a href="#">
+              <span>Կարդալ ավելին</span>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="14"
+                  viewBox="0 0 24 14"
+                  fill="none"
+                >
+                  <path
+                    d="M0 7.00002C0 7.26523 0.105357 7.51959 0.292893 7.70712C0.48043 7.89466 0.734784 8.00002 1 8.00002H20.586L16.294 12.292C16.1118 12.4806 16.011 12.7332 16.0133 12.9954C16.0156 13.2576 16.1208 13.5084 16.3062 13.6938C16.4916 13.8792 16.7424 13.9844 17.0046 13.9867C17.2668 13.989 17.5194 13.8882 17.708 13.706L23.708 7.70602C23.8005 7.61402 23.874 7.50464 23.9241 7.38417C23.9742 7.26369 24 7.1345 24 7.00402V7.00002C24 6.87002 23.974 6.74002 23.922 6.61802C23.8722 6.49678 23.7987 6.38665 23.706 6.29402L17.706 0.294017C17.5174 0.111859 17.2648 0.011065 17.0026 0.0133435C16.7404 0.0156219 16.4896 0.120791 16.3042 0.306199C16.1188 0.491607 16.0136 0.74242 16.0113 1.00462C16.009 1.26681 16.1098 1.51942 16.292 1.70802L20.586 6.00002H1C0.734784 6.00002 0.48043 6.10537 0.292893 6.29291C0.105357 6.48045 0 6.7348 0 7.00002Z"
+                    fill="#3E4095"
+                  />
+                </svg>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.newCollection}>
+        <h3>Գործող առաջարկներ</h3>
+        <div className={styles.productSections}>
+          <div className={styles.slider}>
+            <Carousel
+              className={styles.carousel}
+              responsive={responsive}
+              autoPlay={true}
+              swipeable={true}
+              draggable={true}
+              showDots={false}
+              infinite={true}
+              autoPlaySpeed={3000}
+              partialVisible={false}
+              dotListClass="custom-dot-list-style"
+            >
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+            </Carousel>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import conselLogo from "../../assets/consel_logo 2.png";
+import logoMob from "../../assets/consel_logo 1 (1).png";
+import styles from "./Logo.module.scss";
 
 const Logo = () => {
   return (
     <a href="#">
-      <img src={conselLogo} alt="" />
+      <img className={styles.deskLogo} src={conselLogo} alt="" />
+      <img className={styles.mobLogo} src={logoMob} alt="" />
     </a>
   );
 };
